bd0531ee202ed3e4d6ce594eff86424ae560c101
Jan Machala <jan.machala@email.cz>
2013-03-10 19:27:12 +0100

    Prototype of get method

--- src/CarRegistry/ApiBundle/Controller/OwnerController.php
+++ src/CarRegistry/ApiBundle/Controller/OwnerController.php
@@ -3,18 +3,30 @@
 namespace CarRegistry\ApiBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
- * @Route(service="controller.owner_controller");
+ * @Route("/api/v1/owner", service="controller.owner_controller");
  */
 class OwnerController {
 
 	/**
-	 * @Route("/", name="_demo")
+	 * @Route("/")
+	 * @Method("GET")
 	 */
 	public function indexAction() {
-		return new JsonResponse(array('Hello world'));
+		$owners = array(
+			array(
+				'id' => 1,
+				'name' => 'John Doe',
+			),
+			array(
+				'id' => 2,
+				'name' => 'Arthur Dent',
+			),
+		);
+		return new JsonResponse($owners);
 	}
 
 }
