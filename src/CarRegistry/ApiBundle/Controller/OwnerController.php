3742e5274e6713ce26e7aa8e59a1eb11a0063b77
Jan Machala <jan.machala@email.cz>
2013-03-10 20:32:30 +0100

    Post method for creating new owners

--- src/CarRegistry/ApiBundle/Controller/OwnerController.php
+++ src/CarRegistry/ApiBundle/Controller/OwnerController.php
@@ -3,9 +3,14 @@
 namespace CarRegistry\ApiBundle\Controller;
 
 use CarRegistry\ApiBundle\DAO\OwnerDAO;
+use Doctrine\DBAL\DBALException;
+use CarRegistry\ApiBundle\Entity\Owner;
+use CarRegistry\ApiBundle\JsonDecoder;
+use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/api/v1/owner", service="controller.owner_controller");
@@ -13,9 +18,11 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class OwnerController {
 
 	private $ownerDAO;
+	private $jsonDecoder;
 
-	public function __construct(OwnerDAO $ownerDAO) {
+	public function __construct(OwnerDAO $ownerDAO, JsonDecoder $jsonDecoder) {
 		$this->ownerDAO = $ownerDAO;
+		$this->jsonDecoder = $jsonDecoder;
 	}
 
 	/**
@@ -23,7 +30,33 @@ class OwnerController {
 	 * @Method("GET")
 	 */
 	public function getAction() {
-		return new JsonResponse($this->ownerDAO->getAll());
+		return $this->response($this->ownerDAO->getAll());
+	}
+
+	/**
+	 * @Route("/")
+	 * @Method("POST")
+	 */
+	public function postAction(Request $request) {
+		$owner = new Owner();
+		$this->jsonDecoder->decodeAndFill($request->getContent(), $owner);
+
+		try {
+			$this->ownerDAO->save($owner);
+			$responseData = array('id' => $owner->getId());
+		} catch (DBALException $ex) {
+			$responseData = array('error' => preg_match('~Duplicate entry~', $ex->getMessage()) ? 'Duplicate entry' : 'Unknown error occurred');
+		} catch (Exception $ex) {
+			$responseData = array('error' => 'Unknown error occurred');
+		}
+
+		return $this->response($responseData);
+	}
+
+	private function response($responseData) {
+		return new JsonResponse(array(
+			'response' => $responseData,
+		));
 	}
 
 }
